2992e37174dd39904f5ab6790c73c4b2c46fd897
Slava Cherkesov <cherkesov.vyacheslav@e-queo.com>
2022-09-19 17:51:19 +0300

    feat(useless-switch): Move report messages to constants

---
+++ src/constants/report-messages.ts
@@ -0,0 +1,5 @@
+export const MESSAGES = {
+  IfStatement: 'Avoid multiple logical expression in {{ identifier }}',
+  ReturnStatement: 'Avoid multiple logical expression in {{ identifier }}',
+  SwitchStatement: 'Useless empty case statements.',
+};
--- src/helpers/report.ts
+++ src/helpers/report.ts
@@ -1,15 +1,10 @@
 import { Rule } from 'eslint';
 import type { NodeType } from '../types';
-
-const MESSAGES_MAP = {
-  IfStatement: 'Avoid multiple logical expression in {{ identifier }}',
-  ReturnStatement: 'Avoid multiple logical expression in {{ identifier }}',
-  SwitchStatement: 'Useless empty case statements.',
-};
+import { MESSAGES } from '../constants/report-messages';
 
 export function report(context: Rule.RuleContext, node: NodeType) {
   context.report({
-    message: MESSAGES_MAP[node.type],
+    message: MESSAGES[node.type],
     node,
     data: {
       identifier: node.type,
